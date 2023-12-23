39fd58f1922b2d237742b71013d7f7a7f6891fe5
Dan <danil.pestov@yandex.ru>
2023-12-23 14:05:00 +0300

    1

--- MVVM.01/Views/InfoViewController.swift
+++ MVVM.01/Views/InfoViewController.swift
@@ -12,23 +12,34 @@ class InfoViewController: UIViewController {
     @IBOutlet var cityLabel: UILabel!
     @IBOutlet var ageLabel: UILabel!
     @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var deleteButton: UIButton!
     
     var user = (User.logins[0].age != nil) || (User.logins[0].city != nil) || (User.logins[0].email != nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
         nameLabel.text = "Имя: \(User.logins[0].login ?? "")"
         cityLabel.text = "Город: \(User.logins[0].city ?? "")"
         ageLabel.text = "Возраст: \(User.logins[0].age ?? "")"
         emailLabel.text = "Email: \(User.logins[0].email ?? "")"
-    }
-    @IBAction func yesButtonPressed(_ sender: Any) {
+        
+        setDeleteButton()
     }
     
-    @IBAction func noButtonPressed(_ sender: Any) {
+//    override func viewDidAppear(_ animated: Bool) {
+//        nameLabel.text = "Имя: \(User.logins[0].login ?? "")"
+//        cityLabel.text = "Город: \(User.logins[0].city ?? "")"
+//        ageLabel.text = "Возраст: \(User.logins[0].age ?? "")"
+//        emailLabel.text = "Email: \(User.logins[0].email ?? "")"
+//    }
+    
+    @IBAction func deleteButtonPressed(_ sender: Any) {
+    }
+}
+
+extension InfoViewController {
+    func setDeleteButton() {
+        deleteButton.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        deleteButton.layer.cornerRadius = 10
     }
 }
